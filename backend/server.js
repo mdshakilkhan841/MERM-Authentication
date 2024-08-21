50d5e97789c62fbf66fd8cba1bcef33f41d51936
mdshakilkhan841 <mdshakilkhan841@gmail.com>
2024-08-22 04:22:29 +0600

    fix the cors error

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import { connectDb } from "./database/connectDB.js";
 import authRoute from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
@@ -8,6 +9,10 @@ dotenv.config();
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+
+app.use(cors({
+    origin: "http://localhost:5173", credentials: true
+}));
 
 app.use(express.json()); // Middleware to parse JSON data
 app.use(cookieParser()); // Middleware to parse cookies
