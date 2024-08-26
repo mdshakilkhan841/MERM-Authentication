528b45f8b2813ce9d810d0f7750bcb203aba4323
mdshakilkhan841 <mdshakilkhan841@gmail.com>
2024-08-26 06:45:21 +0600

    MERN Authentication Final deploy

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 import axios from "axios";
 
-const API_URL = "http://localhost:3000/api/auth";
+const API_URL = "https://mern-authentication.onrender.com";
 
 axios.defaults.withCredentials = true;
 
