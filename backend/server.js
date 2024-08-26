766b352ba2bbb33d54c129e2176fe826eb04e5b5
mdshakilkhan841 <mdshakilkhan841@gmail.com>
2024-08-26 22:45:18 +0600

    update the cors policy

--- backend/server.js
+++ backend/server.js
@@ -12,9 +12,9 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-// app.use(cors({
-//     origin: "http://localhost:5173", credentials: true
-// }));
+app.use(cors({
+    origin: "https://jwt-mern-authentication.netlify.app", credentials: true
+}));
 
 app.use(express.json()); // Middleware to parse JSON data
 app.use(cookieParser()); // Middleware to parse cookies
